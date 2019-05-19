f0462ae4c7decd0bb011f0cddbcaf2044f7e6aeb
Austin Cary <acary@gatessolutions.com>
2019-05-19 12:55:03 -0500

    Created project with browserify, ml-pca, typescript, jasmine. Converted data. Ran PCA. Created unlabeled results that really aren't very helpful.

---
+++ src/index.ts
@@ -0,0 +1,28 @@
+var { PCA } = require('ml-pca');
+
+// // dataset is a two-dimensional array where rows represent the samples and columns the features
+const dataset: number[][] = [];
+// const individual = individualStats[0];
+for (let individual of individualStats) {
+  const sample: number[] = [];
+  for (let prop of Object.entries(individual)) {
+    // ["baz", 42]
+    switch (prop[0]) {
+      case 'teamName':
+      case 'year':
+      case 'name':
+        break;
+      default:
+        sample.push(prop[1] !== null ? +prop[1] : 0);
+        break;
+    }
+  }
+  dataset.push(sample);
+}
+console.log(dataset);
+
+const pca = new PCA(dataset);
+console.log(pca.getExplainedVariance());
+console.log(pca.toJSON());
+// const newPoints = [[4.9, 3.2, 1.2, 0.4], [5.4, 3.3, 1.4, 0.9]];
+// console.log(pca.predict(newPoints)); // project new points into the PCA space
